d70e5a21af3786054974e4543804103308b71ec2
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-04-12 19:07:05 -0300

    fix commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# python-primeros-pasos
-Aprendiendo Python
---
+++ proyecto1.py
@@ -0,0 +1,7 @@
+print("Hola")
+
+from datetime import datetime
+
+print(datetime.now())
+print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
+
