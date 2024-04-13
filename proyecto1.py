9a3198b3e54a9ba8ca82a37517ab1d5766af00f2
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-04-13 13:58:10 -0300

    funciones os

--- proyecto1.py
+++ proyecto1.py
@@ -1,7 +1,16 @@
-print("Hola")
-
 from datetime import datetime
+import os
+
 
 print(datetime.now())
 print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
 
+#Result
+# 2024-04-13 13:55:58.126011
+# 13-04-2024 13:55:58
+
+archivo ="D:/busqueda.xlsx"
+nombre,extension=os.path.splitext(archivo)
+print(nombre,extension)
+#Result  
+#D:/busqueda
