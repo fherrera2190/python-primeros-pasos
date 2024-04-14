db580aad3bc0ded76c6efcc659dfe214e423fb85
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-04-14 16:33:10 -0300

    subprocess

--- proyecto1.py
+++ proyecto1.py
@@ -1,17 +1,18 @@
 from datetime import datetime
 import os
+import re
 
 
-print(datetime.now())
-print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
+# print(datetime.now())
+# print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
 
 #Result
 # 2024-04-13 13:55:58.126011
 # 13-04-2024 13:55:58
 
-archivo ="D:/busqueda.xlsx"
-nombre,extension=os.path.splitext(archivo)
-print(nombre,extension)
+# archivo ="D:/busqueda.xlsx"
+# nombre,extension=os.path.splitext(archivo)
+# print(nombre,extension)
 #Result  
 #D:/busqueda
 
@@ -22,6 +23,26 @@ print(nombre,extension)
 #     print("No es igual")
 
 #Eliminar el .
-numero = "13.900"
-numero_sin_punto = numero.replace(".", "")
-print(numero_sin_punto)
+# numero = "13.900"
+# numero_sin_punto = numero.replace(".", "")
+# print(numero_sin_punto)
+
+#expresion regular Filtrar xml
+xml="""<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><soap:Body><FahrenheitToCelsiusResponse xmlns="https://www.w3schools.com/xml/"><FahrenheitToCelsiusResult>37.7777777777778</FahrenheitToCelsiusResult></FahrenheitToCelsiusResponse></soap:Body></soap:Envelope>"""
+#Cuando tiene salto de linea
+#re.search("<FahrenheitToCelsiusResult>(.+?)</FahrenheitToCelsiusResult>","""{res}""").group(1)
+
+result=re.search("<FahrenheitToCelsiusResult>(.+?)</FahrenheitToCelsiusResult>",xml).group(1)
+print(result)
+
+
+import subprocess
+
+# Iniciar un proceso para ejecutar el comando 'ls' y capturar la salida
+proceso = subprocess.Popen(['ls', '-l'], stdout=subprocess.PIPE)
+
+# Capturar la salida del proceso
+salida, errores = proceso.communicate()
+
+# Imprimir la salida del comando
+print(salida.decode())
