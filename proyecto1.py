03be7bf64e0f854b2a6d580fc15de39f504163b9
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-04-19 20:42:33 -0300

    tipos de for

--- proyecto1.py
+++ proyecto1.py
@@ -69,9 +69,39 @@ print(pricelist)
 
 palabra = None
 
+#if None
 if palabra is not None:
     print(palabra)
     print("No Es None","xxxxxxxxxxxxxxxx")
 else:
     print(palabra)
     print("Es None",">>>>>>>>>>>>>>>>")
+
+#for
+
+#for i in range(0,100):
+ #   print (i)
+
+lista = [1, 2, 3, 4, 5]
+for elemento in lista:
+    print(elemento)
+
+tupla = (1, 2, 3, 4, 5)
+for elemento in tupla:
+    print(elemento)
+
+diccionario = {"a": 1, "b": 2, "c": 3}
+for clave in diccionario:
+    print(clave, diccionario[clave])
+
+diccionario = {"a": 1, "b": 2, "c": 3}
+for clave, valor in diccionario.items():
+    print(clave, valor)
+
+conjunto = {1, 2, 3, 4, 5}
+for elemento in conjunto:
+    print(elemento)
+
+cadena = "Hola"
+for caracter in cadena:
+    print(caracter)
